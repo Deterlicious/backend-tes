2c4e976f218159ba00a45e80f30177363f6cc988
Yoga Pradana <132411206+weaknezzy@users.noreply.github.com>
2026-04-17 15:00:09 +0700

    perubahan namaLokasi ke nama tipe

--- services/transferStokService.js
+++ services/transferStokService.js
@@ -108,8 +108,8 @@ class TransferStokService {
 
     try {
       const transfer = await TransferStok.findOne({ _id: id, tenantID })
-        .populate("dariLocationID", "namaLokasi")
-        .populate("keLocationID", "namaLokasi")
+        .populate("dariLocationID", "nama tipe")
+        .populate("keLocationID", "nama tipe")
         .populate("pengirimID", "nama")
         .populate("penerimaID", "nama")
         .populate("items.bahanBakuID", "namaBahan satuan");
